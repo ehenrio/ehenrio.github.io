caa017ed54538a2e3ca31da1fe79f21db4094ef7
thierry <thierry.henrio@gmail.com>
2015-09-07 22:36:57 +0200

    remove, extract, rename
    late was almost drowned in shitte

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 
 // globals
 // =======
-var 
+var
   frontmatter = require( 'gulp-front-matter' ),
   gulp = require( 'gulp' ),
   markdown = require( 'gulp-markdown' ),
@@ -24,26 +24,42 @@ var site = {
   posts: []
 }
 
-// functions
-// ========
-function slugify( s ) {
-  return slug( s )
-}
-
-function may_be_add_basename_as_slug( vinyl ) {
+// helpers
+// =======
+function may_be_add_title_as_slug( vinyl ) {
   if( !vinyl.data.slug ) {
-    vinyl.data.slug = slugify( vinyl.data.title )
+    vinyl.data.slug = slug( vinyl.data.title )
   }
   return vinyl
 }
 
-function may_be_add_resume( vinyl ) {
-  if( !vinyl.data.resume ) {
-    vinyl.data.slug = slugify( vinyl.data.title )
-  }
-  return vinyl
+// vinyl streams
+// =============
+function rename_to_slug() {
+  return through2.obj( function ( f, _encoding, cb ) {
+    // slug is slug or slug( title )
+    may_be_add_title_as_slug( f )
+    // rename basename to slug
+    f.path = path.join( f.base, f.data.slug + path.extname( f.path ) )
+    cb( undefined, f )
+  })
 }
 
+function layout() {
+  return through2.obj( function ( f, _encoding, cb ) {
+    f.contents = new Buffer( nunjucks.render( 'post.html', { contents: f.contents } ) )
+    cb( undefined, f )
+  })
+}
+
+function taint( site ) {
+  return through2.obj( function ( f, _encoding, cb ) {
+    // write to global II
+    f.data.href = f.path
+    site.posts.push( f.data )
+    cb( undefined, f )
+  })
+}
 
 // tasks
 // =====
@@ -64,24 +80,11 @@ gulp.task( 'scripts', function () {
 gulp.task( 'posts', function () {
   return gulp.src( './contents/posts/*.md' )
     .pipe( frontmatter( { property: 'data' } ) )
+    .pipe( rename_to_slug() )
     .pipe( markdown() )
-    // rename basename to slug
-    .pipe( through2.obj( function ( f, _encoding, cb ) {
-      may_be_add_basename_as_slug( f )
-      f.path = path.join( f.base, f.data.slug + path.extname( f.path ) )
-      cb( undefined, f )
-    }))
-    .pipe( through2.obj( function ( f, _encoding, cb ) {
-      f.contents = new Buffer( nunjucks.render( 'post.html', { contents: f.contents } ) )
-      cb( undefined, f )
-    }))
+    .pipe( layout() )
     .pipe( gulp.dest( './site/posts' ) )
-    .pipe( through2.obj( function ( f, _encoding, cb ) {
-      // write to global II
-      f.data.href = f.path
-      site.posts.push( f.data )
-      cb( undefined, f )
-    }))
+    .pipe( taint( site ) )
 })
 
 gulp.task( 'site', [ 'posts' ], function () {
